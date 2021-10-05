ef7593502d14d52ee35b3caa64998b05fe8da20a
Fahim-BAUST <ashhabf50@gmail.com>
2021-10-05 17:39:52 +0600

    responsive nav bar

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -3,56 +3,77 @@ import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div style={{ backgroundColor: "#00203FFF" }}>
 
-            <img style={{ float: "left", width: "160px", bottom: 5 }} src="https://cdn.steemitimages.com/DQmWspTV26MzQTBkR1FNr8j7HfUYcAaUBupLFGsYwG9VSHr/SMART-removebg-preview.png" alt="" />
+        <nav className="navbar navbar-expand-lg navbar-light " style={{ backgroundColor: "#00203FFF" }}>
+            <div className="container-fluid">
 
-            <div style={{ textAlign: "right", backgroundColor: "#00203FFF", padding: "20px" }}>
-                <NavLink
-                    style={{ textDecoration: "none", padding: "5px", margin: "5px", color: "white" }}
-                    to="/home"
-                    activeStyle={{
-                        fontWeight: "bold",
-                        color: "#ADEFD1FF"
-                    }}
-                >
-                    <i class="fas fa-home"></i> Home
-                </NavLink >
-                <NavLink
-                    style={{ textDecoration: "none", padding: "5px", margin: "5px", color: "white" }}
-                    to="/services"
-                    activeStyle={{
-                        fontWeight: "bold",
-                        color: "#ADEFD1FF"
-                    }}
-                >
-                    <i class="fab fa-servicestack"></i>   Services
-                </NavLink>
-                <NavLink
-                    style={{ textDecoration: "none", padding: "5px", margin: "5px", color: "white" }}
-                    to="/free"
-                    activeStyle={{
-                        fontWeight: "bold",
-                        color: "#ADEFD1FF"
-                    }}
-                >
-                    <i class="fab fa-free-code-camp"></i> Free Services
-                </NavLink>
-                <NavLink
-                    style={{ textDecoration: "none", padding: "5px", margin: "5px", color: "white" }}
-                    to="/about"
-                    activeStyle={{
-                        fontWeight: "bold",
-                        color: "#ADEFD1FF"
-                    }}
-                >
-                    <i class="fas fa-address-card"></i>  About us
-                </NavLink>
+                <img className="navbar-brand" style={{ float: "left", width: "100px", marginLeft: 20 }} src="https://cdn.steemitimages.com/DQmWspTV26MzQTBkR1FNr8j7HfUYcAaUBupLFGsYwG9VSHr/SMART-removebg-preview.png" alt="" />
+                <button className="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse justify-content-end " id="navbarNavAltMarkup">
+                    <div className="navbar-nav ">
 
+                        <NavLink
+                            className="nav-link "
+                            style={{ textDecoration: "none", padding: "5px", margin: "5px", color: "white" }}
+                            to="/home"
+                            activeStyle={{
+                                fontWeight: "bold",
+                                color: "#ADEFD1FF"
+                            }}
+                        >
+                            <i className="fas fa-home"></i> Home
+                        </NavLink >
+
+
+                        <NavLink
+                            className="nav-link"
+                            style={{ textDecoration: "none", padding: "5px", margin: "5px", color: "white" }}
+                            to="/services"
+                            activeStyle={{
+                                fontWeight: "bold",
+                                color: "#ADEFD1FF"
+                            }}
+                        >
+                            <i className="fab fa-servicestack"></i>   Services
+                        </NavLink>
+
+
+                        <NavLink
+                            className="nav-link"
+                            style={{ textDecoration: "none", padding: "5px", margin: "5px", color: "white" }}
+                            to="/free"
+                            activeStyle={{
+                                fontWeight: "bold",
+                                color: "#ADEFD1FF"
+                            }}
+                        >
+                            <i className="fab fa-free-code-camp"></i> Free Services
+                        </NavLink>
+
+
+                        <NavLink
+                            className="nav-link "
+                            style={{ textDecoration: "none", padding: "5px", margin: "5px", color: "white" }}
+                            to="/about"
+                            activeStyle={{
+                                fontWeight: "bold",
+                                color: "#ADEFD1FF"
+                            }}
+                        >
+                            <i className="fas fa-address-card"></i>  About us
+                        </NavLink>
+
+                    </div>
+                </div>
             </div>
+        </nav>
 
 
-        </div>
+
+
+
     );
 };
 
